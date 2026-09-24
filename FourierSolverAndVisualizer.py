1874f29dc636e8ff7cc38bf70a5aa2dbe66efa27
Aayan Khan <aayan.khan02072007@gmail.com>
2026-09-24 19:29:51 +0530

    Wave done

--- FourierSolverAndVisualizer.py
+++ FourierSolverAndVisualizer.py
@@ -1,80 +1,138 @@
 import sympy as sp
-C1,C2,C3,C4,x,y,p,c,t,a,L = sp.symbols("C1 C2 C3 C4 x y p c t a L")
+
+C1, C2, C3, C4, x, y, p, c, t, a, L, lam, b, h = sp.symbols(
+    "C1 C2 C3 C4 x y p c t a L lambda b h"
+)
 n = sp.symbols("n", integer=True, positive=True)
-def fourier_coefficient(f):
-    b_n = 2/L * sp.integrate(f * sp.sin(n*sp.pi*x/L), (x,0,L))
-    return sp.simplify(b_n)
+
 
 class Wave_Equation:
-    X = C1*sp.cos(p*x) + C2*sp.sin(p*x)
-    T = C3*sp.cos(p*c*t) + C4 * sp.sin(p*c*t)
-    general_solution = X*T
-    def __init__(self, boundary_conditions = [], initial_conditions = [], derivative_conditions = []):
+    X = C1 * sp.cos(p * x) + C2 * sp.sin(p * x)
+    T = C3 * sp.cos(p * c * t) + C4 * sp.sin(p * c * t)
+    general_solution = X * T
+
+    def __init__(
+        self,
+        boundary_conditions=[],
+        initial_conditions=[],
+        lower=0,
+        upper=L,
+    ):
+        self.length = upper - lower
         self.boundary_conditions = boundary_conditions
         self.initial_conditions = initial_conditions
-        self.derivative_conditions = derivative_conditions
-        self.X = C1*sp.cos(p*x) + C2*sp.sin(p*x)
-        self.T = C3*sp.cos(p*c*t) + C4 * sp.sin(p*c*t)
-        self.general_solution = self.X * self.T
-        self.unknowns = {C1,C2,C3,C4,p}
-        self.found = set()
-        
+        self.X = Wave_Equation.X
+        self.T = Wave_Equation.T
+        self.general_solution = Wave_Equation.general_solution
+        self.unknowns = {C1, C2, C3, C4, p}
+
     def solve_boundary_conditions(self):
-        for sub,value in self.boundary_conditions:
+        for cond in self.boundary_conditions:
+            sub = cond.x
+            value = cond.value
             expression = self.X.subs(x, sub)
             condition = sp.Eq(expression, value)
             involved = self.unknowns.intersection(expression.free_symbols)
             for variable in involved:
                 solution = sp.solve(condition, variable)
                 if solution:
-                    self.found.add(variable)
                     self.unknowns.remove(variable)
                     if variable == C1 and solution[0] == 0:
                         self.unknowns.remove(C2)
-                        self.found.add(C2)
                     elif variable == C2 and solution[0] == 0:
                         self.unknowns.remove(C1)
-                        self.found.add(C1)
                     if variable == p:
-                        self.X = self.X.subs(variable, n*solution[1])
-                        self.T = self.T.subs(variable, n*solution[1])
+                        self.X = self.X.subs(variable, n * solution[1])
+                        self.T = self.T.subs(variable, n * solution[1])
                     else:
                         self.X = self.X.subs(variable, solution[0])
-        self.general_solution  = self.X * self.T
-        
+        self.general_solution = self.X * self.T
+
     def solve_initial_conditions(self):
-        for condition,value in self.initial_conditions:
-            value = fourier_coefficient(value)
-            if condition == "displacement":
-                self.X = self.X.subs(C2, 1)
-                self.T = self.T.subs(C2, 1)
-                self.general_solution = self.X * self.T
-                expression = self.general_solution.subs(t, 0)
-                coefficient = expression.coeff(sp.sin(n*sp.pi*x/L))
-                equation = sp.Eq(coefficient, value)
-                solution = sp.solve(equation, C3)
-                if solution:
-                    self.T = self.T.subs(C3, solution[0])
-                self.general_solution  = self.X * self.T
-            else:
-                derivative = sp.diff(self.general_solution,t)
-                expression = sp.expand(derivative.subs(t, 0))
-                coefficient = expression.coeff(sp.sin(n*sp.pi*x/L))
-                equation = sp.Eq(coefficient, value)    
-                solution = sp.solve(equation, C4)
-                if solution:
-                    self.T = self.T.subs(C4, solution[0])
-                self.general_solution  = self.X * self.T
-    
+        displacement = 0
+        velocity = 0
+        for cond in self.initial_conditions:
+            if cond.type.lower() == "displacement":
+                displacement = cond.value
+            elif cond.type.lower() == "velocity":
+                velocity = cond.value
+
+        a_n = self.a_n(displacement)
+        b_n = self.b_n(velocity)
+
+        self.X = self.X.subs(C2, 1)
+
+        self.T = self.T.subs(C3, a_n)
+        self.T = self.T.subs(C4, b_n)
+
+        self.general_solution = self.X * self.T
+
+    def a_n(self, displacement):
+        a_n = (
+            2
+            / self.length
+            * sp.integrate(
+                displacement * sp.sin(n * sp.pi * x / self.length), (x, 0, self.length)
+            )
+        )
+        return sp.simplify(a_n)
+
+    def b_n(self, velocity):
+        b_n = (
+            2
+            / (n * sp.pi * c)
+            * sp.integrate(
+                velocity * sp.sin(n * sp.pi * x / self.length), (x, 0, self.length)
+            )
+        )
+        return sp.simplify(b_n)
+
     def solve(self):
         self.solve_boundary_conditions()
         self.solve_initial_conditions()
-        sp.pprint(self.general_solution)         
-        
+        sp.pprint(self.general_solution)
+
+
+class Condition:
+    def __init__(self, condition_type):
+        self.type = condition_type
+
+
+class Boundary_Condition(Condition):
+    def __init__(self, x, value):
+        super().__init__("boundary")
+        self.x = x
+        self.value = value
+
+
+class Initial_Condition(Condition):
+    def __init__(self, type):
+        super().__init__("initial")
+        self.type = type
+
+
+class Displacement_Condition(Initial_Condition):
+    def __init__(self, value):
+        super().__init__("Displacement")
+        self.value = value
+
+
+class Velocity_Condition(Initial_Condition):
+    def __init__(self, value):
+        super().__init__("Velocity")
+        self.value = value
+
+
 def main():
-    wave = Wave_Equation(boundary_conditions=[(0,0),(L,0)], initial_conditions=[("displacement",a*sp.sin(3*sp.pi*x/L)*sp.cos(5*sp.pi*x/L)),("velocity", 0)])
+    wave = Wave_Equation(
+          [Boundary_Condition(0, 0), Boundary_Condition(L, 0)],
+                 [
+                     Displacement_Condition(sp.Function("f")(x)),
+                     Velocity_Condition(0),
+                 ],
+    )
     wave.solve()
-    
+
 
 if __name__ == "__main__":
     main()
